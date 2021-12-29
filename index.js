6e22dd3473503bb86d99b39fbfa7a08c10e1a6bc
Austin Baik <baik.austin@gmail.com>
2021-12-28 16:36:51 -0800

    commit tree 3feb41edf8be345262e2fddb10f5278423d01025

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
-const titleCased = () => {
-  return tutorials
+  "What Does The This Keyword Mean?",
+          "What Is The Constructor OO Pattern?",
+          "Implementing Blockchain Web API",
+          "The Test Driven Development Workflow",
+          "What Is NaN And How Can We Check For It",
+          "What Is The Difference Between StopPropagation And PreventDefault?",
+          "Immutable State And Pure Functions",
+          "What Is The Difference Between == And ===?",
+          "What Is The Difference Between Event Capturing And Bubbling?",
+          "What Is JSONP?"
+]
+function titleCased() {
+  return tutorials.map(title => {
+    const tempTitle = title.split(" ");
+    // console.log(tempTitle)
+    tempTitle.map(newTitle => {
+      // console.log(newTitle)
+       newTitle[0].toUpperCase() + newTitle.substr(1);
+      // console.log(newCap)
+      return newTitle 
+      // return newTitle
+    })
+    // console.log(tempTitle)
+  return tempTitle.join(" ")
+  })
 }
